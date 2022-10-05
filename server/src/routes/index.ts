9e0ef5d3170a441b1ca43cddfc2fb99674426097
EddyPBR <edvaldojunior1310@gmail.com>
2022-10-04 22:15:19 -0300

    :sparkles: add prisma orm

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,8 +3,6 @@ import { Router } from "express";
 const router = Router();
 
 router.use("/", (req, res) => {
-  throw new Error("Erro proposital!");
-
   return res.status(200).json({
     message: "hello world",
   });
