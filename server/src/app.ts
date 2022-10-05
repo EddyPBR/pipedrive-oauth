e9e3b3532fd8211988a3377f70cbe4b4cc2bf7c5
EddyPBR <edvaldojunior1310@gmail.com>
2022-10-04 22:03:56 -0300

    :sparkles: implements errorHandler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
+import "express-async-errors";
 
 import { router } from "~routes/index";
+import { errorHandler } from "~middlewares/errorHandler";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/api", router);
+
+app.use(errorHandler);
 
 export { app };
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.use("/", (req, res) => {
+  throw new Error("Erro proposital!");
+
   return res.status(200).json({
     message: "hello world",
   });
