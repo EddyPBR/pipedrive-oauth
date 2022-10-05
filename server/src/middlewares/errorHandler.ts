be038d67a73226e704e1fc6fb34208b32fae4c0f
EddyPBR <edvaldojunior1310@gmail.com>
2022-10-04 22:17:03 -0300

    :sparkles: add new error handler for prisma & celebrate/joi

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,5 +1,7 @@
+import { isCelebrateError } from "celebrate";
 import type { Request, Response, NextFunction } from "express";
 import { AppException } from "~exceptions/AppException";
+import { Prisma } from "@prisma/client";
 
 export const errorHandler = (
   error: Error,
@@ -9,6 +11,39 @@ export const errorHandler = (
 ) => {
   if (error instanceof AppException) {
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (isCelebrateError(error)) {
+    const types = ["body", "query", "headers", "params"];
+
+    const errors = types.reduce((acc, type) => {
+      const celebrateError = error.details.get(type)?.details[0].message;
+
+      if (!celebrateError) return acc;
+
+      const formattedErrorMessage = celebrateError.replace(/"/g, "");
+
+      return [...acc, formattedErrorMessage];
+    }, [] as string[]);
+
+    return response.status(400).json({
+      message: errors[0] ?? "General validation error", // return only the first error
+    });
+  }
+
+  if (
+    error instanceof Prisma.PrismaClientKnownRequestError ||
+    error instanceof Prisma.PrismaClientUnknownRequestError
+  ) {
+    return response.status(500).json({
+      message: error.message,
+    });
+  }
+
+  if (error?.name === "NotFoundError") {
+    return response.status(404).json({
+      message: "Not found",
+    });
   }
 
   return response.status(500).json({
