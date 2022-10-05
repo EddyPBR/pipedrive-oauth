c6a09af12c2dae8e14b75b4ef14103b478b309c5
EddyPBR <edvaldojunior1310@gmail.com>
2022-10-04 22:26:50 -0300

    :tada: starting project web with nextjs + eslint + prettier + typescript

---
+++ web/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Pipedrive oauth2</title>
+        <meta name="description" content="Pipedrive oauth2 teste" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
